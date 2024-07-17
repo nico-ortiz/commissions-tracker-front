c340c0100901157095ddba670bb9c21826df31fa
nico-ortiz <nicortiz1234@gmail.com>
2024-07-17 11:34:02 -0300

    create routes #4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule),
+  },
+  {
+    path: 'parcels',
+    loadChildren: () => import('./parcels/parcels.module').then(m => m.ParcelsModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'parcels',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
