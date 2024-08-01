e19ea16e86ccf92d143eeab242b89cc4be8fd2c0
nico-ortiz <nicortiz1234@gmail.com>
2024-08-01 17:35:41 -0300

    add animations and styles for side navbar #7

--- src/app/shared/components/side-navbar/side-navbar.component.ts
+++ src/app/shared/components/side-navbar/side-navbar.component.ts
@@ -1,10 +1,25 @@
 import { Component, Input } from '@angular/core';
 import { ToggleButtonService } from '../../services/toggle-button.service';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'shared-side-navbar',
   templateUrl: './side-navbar.component.html',
-  styleUrl: './side-navbar.component.css'
+  styleUrl: './side-navbar.component.css',
+  animations: [
+    trigger(
+      'enterAnimation', [
+        transition(':enter', [
+          style({transform: 'translateX(0)', opacity: 0}),
+          animate('200ms', style({transform: 'translateX(0)', opacity: 1}))
+        ]),
+        transition(':leave', [
+          style({transform: 'translateX(0)', opacity: 1}),
+          animate('200ms', style({transform: 'translateX(0)', opacity: 0}))
+        ])
+      ]
+    )
+  ]
 })
 export class SideNavbarComponent {
 
